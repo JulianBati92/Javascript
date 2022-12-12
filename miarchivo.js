7bb004073a16008740f9225b4d81f5266fb4e490
JulianBati92 <julian.batistutti@gmail.com>
2022-12-12 12:51:21 -0300

    se agrega las ultimas actualizaciones del tp

--- miarchivo.js
+++ miarchivo.js
@@ -1,23 +1,23 @@
- function Saludar () {
-     let nombre = prompt ('Como es tu nombre?');
-     let apellido = prompt ('Como es tu apellido?')
-     alert ("Tomamos unos mates? " + nombre + " " + apellido );
+function Saludar () {
+    let nombre = prompt ('Como es tu nombre?');
+    let apellido = prompt ('Como es tu apellido?')
+    alert ("Tomamos unos mates? " + nombre + " " + apellido );
 
-  let entrada = prompt("Ingresar tus redes sociales");
-  while(entrada != ""){
-    switch (entrada) {
-       case "instagram.com/":
-            alert("Gracias por ingresar tu instagram, tenes un 20% off ingresando el codigo MATTEOLIIG en la tienda online");
-            break;
-       case "facebook.com/":
-            alert("Gracias por ingresar tu facebook, tenes un 20% off ingresando el codigo MATTEOLIFB en la tienda online");
-            break;
-       default:
-           alert("Gracias por tu tiempo!!!")
+ let entrada = prompt("Ingresar tus redes sociales");
+ while(entrada != ""){
+   switch (entrada) {
+      case "instagram.com/":
+           alert("Gracias por ingresar tu instagram, tenes un 20% off ingresando el codigo MATTEOLIIG en la tienda online");
            break;
-   }
-   entrada = prompt("Ingresar tus redes sociales");
-   }
+      case "facebook.com/":
+           alert("Gracias por ingresar tu facebook, tenes un 20% off ingresando el codigo MATTEOLIFB en la tienda online");
+           break;
+      default:
+          alert("Gracias por tu tiempo!!!")
+          break;
+  }
+  entrada = prompt("Ingresar tus redes sociales");
+  }
 }
 
 Saludar ()
@@ -30,21 +30,21 @@ class Productos {
         this.precio = precio;
     }
 }
-const listaProductos = [
-    {mate: "set completo", modelo: "set", precio: 10000, stock:3},
-    {mate: "plata 925 crem", modelo: "imperial", precio: 2850, stock: 8},
-    {mate: "plata 925 blanco", modelo: "imperial", precio: 2600, stock: 15},
-    {mate: "rosalia", modelo: "imperial", precio: 2500, stock:10},
-    {mate: "beige", modelo: "torpedo", precio: 2200, stock: 22},
-    {mate: "blanco", modelo: "imperial", precio: 2500, stock: 7},
-    {mate: "cream", modelo: "camionero", precio: 2300, stock:18},
-    {mate: "negro", modelo: "imperial", precio: 2500, stock: 1},
-    {mate: "alpaca", modelo: "bombilla", precio: 2500, stock: 45},
-    {mate: "acero", modelo: "bombilla", precio: 2500, stock: 60},
+let productosAgregados = [
+    {id:1, mate: "set completo", modelo: "set", precio: 1000, stock:3},
+    {id:2, mate: "plata 925 crem", modelo: "imperial", precio: 2850, stock: 8},
+    {id:3, mate: "plata 925 blanco", modelo: "imperial", precio: 2600, stock: 15},
+    {id:4, mate: "rosalia", modelo: "imperial", precio: 2500, stock:10},
+    {id:5, mate: "beige", modelo: "torpedo", precio: 2200, stock: 22},
+    {id:6, mate: "blanco", modelo: "imperial", precio: 2500, stock: 7},
+    {id:7, mate: "cream", modelo: "camionero", precio: 2300, stock:18},
+    {id:8, mate: "negro", modelo: "imperial", precio: 2500, stock: 1},
+    {id:9, mate: "alpaca", modelo: "bombilla", precio: 1900, stock: 23},
+    {id:10, mate: "acero", modelo: "bombilla", precio: 1500, stock: 40},
 ];
 
 listaProductos.forEach((producto)=> {
-    console.log(`este mate es ${producto.modelo} ${producto.mate} y el precio es $${producto.precio}`)
+    console.log(`este ${producto.modelo} ${producto.mate} y el precio es $${producto.precio}`)
 })
 
 let busquedaUsuario = prompt("que modelo queres buscar?")
@@ -54,3 +54,31 @@ console.log (busqueda)
 let filtraPorPrecio = prompt("que precio?")
 const filtra = listaProductos.filter(producto => producto.precio > 1500);
 console.log (filtra)
+
+let crearProducto = () => {
+    let mate = document.querySelector("#mate").value;
+    let modelo = document.querySelector("#modelo").value;
+    let precio = parseInt(document.querySelector("#precio").value);
+
+    let mateNuevo = new Productos(mate, modelo, precio);
+    productosAgregados.push(mateNuevo);
+    return productosAgregados
+}
+
+let pintarHTML = () => {
+    productosAgregados.forEach(producto => {
+        if (producto.stock > 0) {
+            let contenedor = document.createElement("div");
+            contenedor.setAttribute("id", producto.id);
+            contenedor.innerHTML = `<h3> ${producto.mate}</h3>
+                              <p> ${producto.modelo}</p>
+                              <b> $${producto.precio}</b>
+                            `;
+            document.getElementById("div").appendChild(contenedor);
+        } else {
+            document.getElementById("div").innerHTML += `<p>Huy, justo ese no queda ${producto.mate}</p>`
+        }
+    })
+}
+
+
