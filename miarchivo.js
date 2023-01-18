d1d9e127d2b09d6ca216a022319f6e131e0e3493
JulianBati92 <113951097+JulianBati92@users.noreply.github.com>
2023-01-17 21:11:09 -0300

    Se agregan funciones de orden superior
    Se agregan funciones de orden superior

--- miarchivo.js
+++ miarchivo.js
@@ -13,120 +13,118 @@ class Producto {
 
 //Creo productos y los almaceno en un array:
 
-const producto1 = new Producto(1, 'set completo matero', 1000, 3);
-const producto2 = new Producto(2, 'imperial 925 cream', 2850, 8);
-const producto3 = new Producto(3, 'imperial 926 blanco', 2600, 15);
-const producto4 = new Producto(4, 'imperial rosalia', 2500, 10);
-const producto5 = new Producto(5, 'torpedo beige', 2200, 22);
-const producto6 = new Producto(6, 'imperial blanco', 2500, 7);
-const producto7 = new Producto(7, 'camionero cream', 2300, 18);
-const producto8 = new Producto(8, 'imperial negro', 2500, 1);
-const producto9 = new Producto(9, 'bombilla alpaca', 1900, 23);
-const producto10 = new Producto(10,'bombilla acero', 1500, 40);
-
-const productos = [producto1, producto2, producto3, producto4, producto4, producto5, producto6, producto7, producto8, producto9, producto10];
+const productos = [
+    new Producto(1, 'set completo matero', 1000, 3),
+    new Producto(2, 'imperial 925 cream', 2850, 8),
+    new Producto(3, 'imperial 926 blanco', 2600, 15),
+    new Producto(4, 'imperial rosalia', 2500, 10),
+    new Producto(5, 'torpedo beige', 2200, 22),
+    new Producto(6, 'imperial blanco', 2500, 7),
+    new Producto(7, 'camionero cream', 2300, 18),
+    new Producto(8, 'imperial negro', 2500, 1),
+    new Producto(9, 'bombilla alpaca', 1900, 23),
+    new Producto(10,'bombilla acero', 1500, 40),
+];
 
 // Almacenar los productos en el local storage:
+
 localStorage.setItem('productos', JSON.stringify(productos));
 
 // Recuperar el objeto del local storage:
-const miProductoRecuperado = JSON.parse(localStorage.getItem('producto'));
+
+const productosRecuperados = JSON.parse(localStorage.getItem('productos'));
 
 //Muestro los productos modificando el DOM.
 
 const contenedorProductos = document.getElementById('contenedorProductos');
-
-productos.forEach((producto) => {
-  const divProducto = document.createElement('div');
-  divProducto.classList.add('card', 'col-xl-3', 'col-md-6', 'col-sm-12');
-  divProducto.innerHTML = `
-                          <div>
-                              <img src="img/${producto.id}.jpg" class="card-img-top img-fluid py-3">
-                              <div class="card-body">
-                                  <h3 class="card-title"> ${producto.nombre} </h3>
-                                  <p class="card-text"> ${producto.precio} </p>
-                                  <button id="boton${producto.id}" class="btn btn-primary"> Agregar al Carrito </button>
-                              </div>
-                          </div>`;
-  contenedorProductos.appendChild(divProducto);
-
-  //Agregar un evento al boton de agregar al carrito:
-  const boton = document.getElementById(`boton${producto.id}`);
-  boton.addEventListener('click', () => {
-    agregarAlCarrito(producto.id);
-  });
-});
-
-//Creo el carrito de compras y una función que busque el producto por id y lo agregue al carrito.
-
-const carrito = [];
-
-//chequea las cantidades:
-
-const agregarAlCarrito = (id) => {
-  const producto = productos.find((producto) => producto.id === id);
-  const productoEnCarrito = carrito.find((producto) => producto.id === id);
-  if (productoEnCarrito) {
-    productoEnCarrito.cantidad++;
-  } else {
-    carrito.push(producto);
-  }
-  actualizarCarrito();
-};
-
-//Muestro el carrito de compras modificando el DOM.
-
 const contenedorCarrito = document.getElementById('contenedorCarrito');
-const verCarrito = document.getElementById('verCarrito');
-
-verCarrito.addEventListener('click', actualizarCarrito);
-
-function actualizarCarrito() {
-  let aux = '';
-  carrito.forEach((producto) => {
-    aux += `
-              <div class="card col-xl-3 col-md-6 col-sm-12">
-                  <img src="img/${producto.id}.jpg" class="card-img-top img-fluid py-3">
-                  <div class="card-body">
-                      <h3 class="card-title"> ${producto.nombre} </h3>
-                      <p class="card-text"> ${producto.precio} </p>
-                      <button onClick = "eliminarDelCarrito(${producto.id})" class="btn btn-primary"> Eliminar del Carrito </button>
-                  </div>
-              </div>
-              `;
-  });
-
-  contenedorCarrito.innerHTML = aux;
-  calcularTotalCompra();
-
-  // Guardar el carrito en local storage:
-  storage.setItem('carrito', JSON.stringify(carrito));
-}
-
-//Agrego una función que elimine el producto del carrito:
-
-const eliminarDelCarrito = (id) => {
-  const producto = carrito.find((producto) => producto.id === id);
-  carrito.splice(carrito.indexOf(producto), 1);
-  actualizarCarrito();
-};
-
-///Función para vaciar todo el carrito por completo:
-
-const vaciarCarrito = document.getElementById('vaciarCarrito');
-vaciarCarrito.addEventListener('click', () => {
-  carrito.splice(0, carrito.length);
-  actualizarCarrito();
-});
-
-//Creo una función que me calcule el total del carrito:
-
+const verCarritoBtn = document.getElementById('verCarrito');
+const vaciarCarritoBtn = document.getElementById('vaciarCarrito');
 const totalCompra = document.getElementById('totalCompra');
 
-const calcularTotalCompra = () => {
-  let totalCompra = 0;
-  carrito.forEach((producto) => {
-    totalCompra += producto.precio;
+let carrito = [];
+let total = 0;
+
+// Crea un div para cada producto en el array de productos.
+
+function crearDivProductos(productos) {
+  return productos.map((producto) => {
+    const divProducto = document.createElement('div');
+    divProducto.classList.add('card', 'col-xl-3', 'col-md-6', 'col-sm-12');
+    divProducto.innerHTML = `
+    <div>
+        <img src="img/${producto.id}.jpg" class="card-img-top img-fluid py-3">
+        <div class="card-body">
+            <h5 class="card-title">${producto.nombre}</h5>
+            <p class="card-text">$ ${producto.precio}</p>
+            <button class="btn btn-primary" onclick="agregarAlCarrito(${producto.id})">Agregar al Carrito</button>
+        </div>
+    </div>`;
+    return divProducto;
   });
-  document.getElementById('totalCompra').innerHTML = `$${totalCompra}`;
-};
+}
+
+// Muestra los productos en el contenedor de productos.
+
+function mostrarProductos() {
+    contenedorProductos.innerHTML = "";
+    const divsProductos = crearDivProductos(productosRecuperados);
+    divsProductos.forEach((divProducto) => {
+        contenedorProductos.appendChild(divProducto);
+    });
+}
+
+
+// Buscar producto en el array de productos recuperados y agrga al carrito actualizando total de compra.
+
+function agregarAlCarrito(id) {
+  const producto = productosRecuperados.find(p => p.id === id);
+  carrito.push(producto);
+  total += producto.precio;
+  totalCompra.innerHTML = total;
+}
+
+// Recorre el array de carrito para crear una vista para cada producto.
+
+function mostrarCarrito() {
+contenedorCarrito.innerHTML = "";
+carrito.forEach((producto) => {
+    const divProducto = document.createElement('div');
+    divProducto.classList.add('card', 'col-xl-3', 'col-md-6', 'col-sm-12');
+    divProducto.innerHTML = `
+    <div>
+        <img src="img/${producto.id}.jpg" class="card-img-top img-fluid py-3">
+        <div class="card-body">
+            <h5 class="card-title">${producto.nombre}</h5>
+            <p class="card-text">$ ${producto.precio}</p>
+            <button class="btn btn-danger" onclick="quitarDelCarrito(${producto.id})">Quitar del Carrito</button>
+        </div>
+    </div>`;
+    contenedorCarrito.appendChild(divProducto);
+});
+}
+
+// Buscar producto en el array de carrito, reduce precio, elimina producto de carrito y actualiza.
+
+function quitarDelCarrito(id) {
+const productoIndex = carrito.findIndex(p => p.id === id);
+total -= carrito[productoIndex].precio;
+totalCompra.innerHTML = total;
+carrito.splice(productoIndex, 1);
+mostrarCarrito();
+}
+
+// Vaciar array de carrito.
+
+function vaciarCarrito() {
+carrito = [];
+total = 0;
+totalCompra.innerHTML = total;
+mostrarCarrito();
+}
+
+// Establecer eventos del mouse para los botones.
+
+verCarritoBtn.addEventListener("click", mostrarCarrito);
+vaciarCarritoBtn.addEventListener("click", vaciarCarrito);
+mostrarProductos();
