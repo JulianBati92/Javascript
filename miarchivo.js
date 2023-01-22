8da3cfc61947adc20f926b7a7170b3274c1c573e
JulianBati92 <113951097+JulianBati92@users.noreply.github.com>
2023-01-22 19:44:06 -0300

    Update miarchivo.js

--- miarchivo.js
+++ miarchivo.js
@@ -77,16 +77,15 @@ function getProductosFromAPI() {
 
 // Crea un div para cada producto en el array de productos.
 
+// Crea un div para cada producto en el array de productos.
+
 function crearDivProductos(productos) {
-  if(!productos || productos.length === 0){
-    return;
-  }
   return productos.map((producto) => {
     const divProducto = document.createElement('div');
     divProducto.classList.add('card', 'col-xl-3', 'col-md-6', 'col-sm-12');
     divProducto.innerHTML = `
     <div>
-        <img src="${producto.imagen}" class="card-img-top img-fluid py-3">
+        <img src="img/${producto.id}.jpg" class="card-img-top img-fluid py-3">
         <div class="card-body">
             <h5 class="card-title">${producto.nombre}</h5>
             <p class="card-text">$ ${producto.precio}</p>
@@ -100,75 +99,75 @@ function crearDivProductos(productos) {
 // Muestra los productos en el contenedor de productos.
 
 function mostrarProductos() {
-  contenedorProductos.innerHTML = "";
-  if(!productosRecuperados || productosRecuperados.length === 0){
-    return;
-  }
-  const divsProductos = crearDivProductos(productosRecuperados);
-  divsProductos.forEach((divProducto) => {
-  contenedorProductos.appendChild(divProducto);
-  });
+    contenedorProductos.innerHTML = "";
+    const divsProductos = crearDivProductos(productosRecuperados);
+    divsProductos.forEach((divProducto) => {
+        contenedorProductos.appendChild(divProducto);
+    });
 }
+
 
 // Buscar producto en el array de productos recuperados y agrga al carrito actualizando total de compra. Muestra si no hay stock del producto
 
 function agregarAlCarrito(id) {
-const producto = productos.find(p => p.id === id);
-if (producto.cantidad > 0) {
-  producto.cantidad -= 1;
-  carrito.push(producto);
-  total += producto.precio;
-  totalCompra.innerHTML = total;
-} else {
-  alert("No hay mas stock de este Matteoli");
-}
+  const producto = productosRecuperados.find(p => p.id === id);
+  if (producto.cantidad > 0) {
+    producto.cantidad -= 1;
+    carrito.push(producto);
+    total += producto.precio;
+    totalCompra.textContent = total;
+  } else {
+    alert("No hay mas stock de este Matteoli");
+  }
 }
 
 // Recorre el array de carrito para crear una vista para cada producto.
 
 function mostrarCarrito() {
-  contenedorCarrito.innerHTML = "";
-  carrito.forEach((producto) => {
-  contenedorCarrito.innerHTML += `
-  <div class="card my-3">
-      <div class="card-body">
-          <h5 class="card-title">${producto.nombre}</h5>
-          <p class="card-text">$ ${producto.precio}</p>
-          <button class="btn btn-danger" onclick="eliminarDelCarrito(${producto.id})">Eliminar</button>
-      </div>
-  </div>`;
-  });
+contenedorCarrito.innerHTML = "";
+carrito.forEach((producto) => {
+    const divProducto = document.createElement('div');
+    divProducto.classList.add('card', 'col-xl-3', 'col-md-6', 'col-sm-12');
+    divProducto.innerHTML = `
+    <div>
+        <img src="img/${producto.id}.jpg" class="card-img-top img-fluid py-3">
+        <div class="card-body">
+            <h5 class="card-title">${producto.nombre}</h5>
+            <p class="card-text">$ ${producto.precio}</p>
+            <button class="btn btn-danger" onclick="quitarDelCarrito(${producto.id})">Quitar del Carrito</button>
+        </div>
+    </div>`;
+    contenedorCarrito.appendChild(divProducto);
+});
+}
+
+// Buscar producto en el array de carrito, reduce precio, elimina producto de carrito y actualiza.
+
+function quitarDelCarrito(id) {
+const productoIndex = carrito.findIndex(p => p.id === id);
+total -= carrito[productoIndex].precio;
+totalCompra.innerHTML = total;
+carrito.splice(productoIndex, 1);
+mostrarCarrito();
 }
 
 // Vaciar array de carrito.
 
 function vaciarCarrito() {
-  carrito = [];
-  total = 0;
-  totalCompra.innerHTML = total;
-  mostrarCarrito();
+carrito = [];
+total = 0;
+totalCompra.innerHTML = total;
+mostrarCarrito();
 }
 
-// Buscar producto en el array de carrito, reduce precio, elimina producto de carrito y actualiza.
+// Establecer eventos del mouse para los botones.
 
-function eliminarDelCarrito(id) {
-  const producto = carrito.find(p => p.id === id);
-  carrito.splice(carrito.indexOf(producto), 1);
-  total -= producto.precio;
-  totalCompra.innerHTML = total;
-  mostrarCarrito();
-}
+verCarritoBtn.addEventListener("click", mostrarCarrito);
+vaciarCarritoBtn.addEventListener("click", vaciarCarrito);
+finalizarCompraBtn.addEventListener('click', finalizarCompra);
+mostrarProductos();
 
 //Finaliza la compra hecha por el cliente.
-
-function finalizarCompra() {
-  if (carrito.length === 0) {
-  alert("No hay productos en el carrito");
-  } else {
-  alert("Compra finalizada. Total: $" + total);
-  vaciarCarrito();
-  }
-}
 
 function finalizarCompra() {
   const carrito = JSON.parse(localStorage.getItem('carrito'));
